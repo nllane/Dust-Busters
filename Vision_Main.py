8a31ccc48f6717ad1f9800d529ad40b406b63b68
nllane <99987383+nllane@users.noreply.github.com>
2023-05-07 20:27:02 -0600

    Update and rename Vision.py to Vision_Main.py

--- Vision_Main.py
+++ Vision_Main.py
@@ -1,4 +1,11 @@
- ##library
+#!/usr/bin/pyton3
+'''
+This is the code for Basic wall following it will only look for obsticals and at a 90 degree angle left of the robot.
+The code also has an attempt to signal the Arduino when the python code stops.
+This code has no ablity correcting based on the angle to the wall.
+'''
+
+##library
 import math
 import numpy as np
 import time
@@ -12,6 +19,7 @@ import os
 ##from adafruit_rplidar import RPLidar
 from rplidar import RPLidar
 ## spare wire to tell the Arduino that the code stopped
+## pi sends 3.3 volts and arduino is a 5 volt system so this was not reliable but did work for a short time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(40,GPIO.OUT,initial=GPIO.HIGH)
 
@@ -58,21 +66,21 @@ def process_data(data):
         if angle > 30 and angle < 270 :## 120 degrees viewed
             continue
 ## checks the Right side for obstacles within 30 degree angles at up to 80 cm
-        if (angle > 340 and distance < 700 and distance > 0):
+        if (angle > 330 and distance < 800 and distance > 0):
             left+=1
 ## checks the left side for obstacles within 30 degree angles at up to 80 cm
-        if (angle < 30 and distance < 700 and distance > 0):
+        if (angle < 30 and distance < 800 and distance > 0):
             right+=1
-## checks the left side of the robot for the closest point
+## checks the left side of the robot for the distance perpendicular to the robot
         if angle == 270:
-            if distance <= wall and distance!=0:
-## adjust angle and distance sent based on shortest distance
+            if distance!=0:
+## sets distance to send to this value
                 wall=distance
                 position=angle
     count +=1
 ## adjusts the value to be in cms
     cm=int((wall)/10)
-    print("wall ",cm)
+    print("wall ",cm)## Used to debug any problems with distance
 ## Creates a value to adjust for bit size
     distance_corrector= int(cm/255)
     print("angle ",position)
@@ -108,12 +116,18 @@ try:
             scan_data[min([359, floor(angle)])] = distance
         process_data(scan_data)
 ## runs the processing code
-except KeyboardInterrupt: ## stop command from computer
+
+except: ## stop command from computer or if it hits an error
+ ## For debugging I would add KeyBoardInterrupt to the above code to allow for the errors to be raised and displayed
     print('Stoping.')
  ##signal that an error ocurred by setting a pin high
     GPIO.output(40,GPIO.HIGH)
+ ## code to properly stop the lidar
     lidar.stop()
     lidar.stop_motor()
+    lidar.clean_input()
+## This makes sure there are no unexpected values already in the lidar
     lidar.disconnect()
     time.sleep(2)
+## gives time for the arduino to see the flag
     GPIO.output(40,GPIO.LOW)
